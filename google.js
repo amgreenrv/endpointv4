62ce915aee5d3bbb491511800e52fc69dcaf0595
Amanda Green <79995097+amgreenrv@users.noreply.github.com>
2022-02-08 18:18:19 -0800

    combine console log

--- google.js
+++ google.js
@@ -4,19 +4,18 @@ let search = 'J.D.+Robb';
 
 
 //Fetch Path
-fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${search}&limit=1`)
+fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${search}&limit=10`)
   .then(response => {
     return response.json();
   })
   .then(data => {
+
   const bookInfo = data.items[1];
 
-  let title = bookInfo.volumeInfo.title;
-  let author = bookInfo.volumeInfo.authors;
-  let publisher = bookInfo.volumeInfo.publisher;
 
-  console.log("Title:" + ' ' + title); 
-  console.log("Authors:" + ' ' + author); 
-  console.log("Publisher:" + ' ' + publisher); 
+  console.log("Title:" + ' ' + bookInfo.volumeInfo.title); 
+  console.log("Authors:" + ' ' + bookInfo.volumeInfo.authors); 
+  console.log("Publisher:" + ' ' + bookInfo.volumeInfo.publisher); 
 
 });
+
